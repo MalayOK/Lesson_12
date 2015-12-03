807454fb1d7fed3a5055e1473fdbb6f180946e54
Aleksandr Malay <malayok@rambler.ru>
2015-12-03 12:02:31 +0200

    Lesson_12

---
+++ Lesson_9_double_array/main.cpp
@@ -0,0 +1,45 @@
+#include <exception>
+#include<iostream>
+#include "SmartArray.h"
+using namespace std;
+using namespace lesson_13;
+int main()
+{
+	SmartArray<int> a;
+	for (int i = 0; i < 100; ++i)
+	{
+		a.Add(3+i);
+		cout << a[i] << endl;
+	}
+	SmartArray<double> b;
+	b.Add(1.2);
+	a.Add(23);
+	a.GetSize();
+	a.Insert(2, 158);
+	a.GetSize();
+	int index = -21;
+	a[index];
+	double x = a[10];
+	cout << x<<endl;
+	try
+	{if (index < 0)
+		throw std::runtime_error("Index cannot be negative"); }
+		catch (myexception& ex)
+	{
+		std::cout << "This is my exception!" << ex.what() << std::endl; 
+	}
+		catch (std::exception& ex)
+	{
+		std::cout << ex.what() << std::endl; 
+	}
+		catch (...)
+	{
+		std::cout << "Catched exception" << std::endl; 
+	}
+    x=a.GetAt(-122);
+	cout << x;
+	int y=a.FindNext(3, 50);
+	cout << y << endl;
+	cin.get();
+	return 0;
+}
